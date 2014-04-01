c520926cd2f2f7829289a7c549a75f01b9033c8a
Jo Liss <joliss42@gmail.com>
2014-04-01 05:11:49 +0100

    Git does not track empty directories

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
+var fs = require('fs')
 var test = require('tap').test
 var walkSync = require('../')
 
 test('walkSync', function (t) {
   t.deepEqual(walkSync('fixtures'), [
-    'dir1/',
-    'dir1/bar.txt',
-    'dir1/subdir/',
-    'dir1/subdir/baz.txt',
-    'dir1/zzz.txt',
-    'dir2/',
+    'dir/',
+    'dir/bar.txt',
+    'dir/subdir/',
+    'dir/subdir/baz.txt',
+    'dir/zzz.txt',
     'foo.txt',
     'symlink1',
     'symlink2'
