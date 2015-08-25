74f5feae5ba97e29039b72bfb739a4ef92e3f069
Stefan Penner <stefan.penner@gmail.com>
2015-08-25 15:03:33 -0700

    JSHINT

--- may-contain.js
+++ may-contain.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Minimatch = require('minimatch').Minimatch;
 
 function MatcherCollection(matchers) {
@@ -34,10 +36,11 @@ MatcherCollection.prototype.mayContain = function(value) {
 function makeArray(x) {
   return Array.isArray(x) ? x : [ x ];
 }
+
 module.exports = function mayContain(value, matcher) {
   return new MatcherCollection(makeArray(matcher)).mayContain(value);
-}
+};
 
 module.exports.buildMatcher = function buildMatcher(matcher) {
   return new MatcherCollection(makeArray(matcher));
-}
+};
